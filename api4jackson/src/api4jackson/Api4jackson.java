03c580e9ad93f9344f2a85ac55525cab259672f5
eldermoraes <elder.moraes@gmail.com>
2015-07-15 18:27:18 -0300

    Project created

---
+++ api4jackson/src/api4jackson/Api4jackson.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api4jackson;
+
+/**
+ *
+ * @author eldermoraes
+ */
+public class Api4jackson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
