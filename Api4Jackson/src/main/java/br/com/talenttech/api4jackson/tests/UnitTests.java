2733a82e5c607d1b152151fd50a03c54a950f1b7
eldermoraes <elder.moraes@gmail.com>
2015-07-16 02:14:49 -0300

    getObjectAsJSONFile and getObjectAsJSONString

--- Api4Jackson/src/main/java/br/com/talenttech/api4jackson/tests/UnitTests.java
+++ Api4Jackson/src/main/java/br/com/talenttech/api4jackson/tests/UnitTests.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 import static java.util.stream.Collectors.toList;
 import java.util.stream.Stream;
@@ -22,14 +23,18 @@ import java.util.stream.Stream;
 public class UnitTests {
     
     public static void main(String[] args) throws IOException{
-        Employee emp = new Employee("John", 5000, "IT", 34);
-        File file = Api4Jackson.getObjectAsJSONFile(emp);
+        Employee emp1 = new Employee("John", 5000, "IT", 34);
+        Employee emp2 = new Employee("Mary", 7500, "HS", 37);
+        List<Employee> listEmp = new ArrayList<>();
+        listEmp.add(emp1);
+        listEmp.add(emp2);
+        File file = Api4Jackson.getObjectAsJSONFile(listEmp);
         
         BufferedReader reader = Files.newBufferedReader(file.toPath(), StandardCharsets.UTF_8);
         List<String> list = reader.lines().flatMap(line -> Stream.of(line)).collect(toList());
         System.out.println("[File]: " + list);
         
-        String jsonString = Api4Jackson.getObjectAsJSONString(emp);
+        String jsonString = Api4Jackson.getObjectAsJSONString(listEmp);
         System.out.println("[String]: " + list);
     }
 }
